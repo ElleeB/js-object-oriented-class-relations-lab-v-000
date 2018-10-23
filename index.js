de014932c7c8782154f63a83c20b40081fb42b0a
ElleeB <laura.trager@gmail.com>
2018-10-23 18:10:34 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,102 @@
+let store = {drivers: [], passengers: [], trips: []}
+let driverId = 0
+let passengerId = 0
+let tripId = 0
+
+///
+
+class Driver {
+
+  constructor(name) {
+    this.name = name
+    this.id = ++driverId
+
+    store.drivers.push(this)
+  }
+
+  trips() {
+    return store.trips.filter (
+      function(trip) {
+        return trip.driverId === this.id
+      }.bind(this)
+    )
+  }
+
+  passengers() {
+    return store.passengers.filter (
+      function(passenger) {
+        return passenger.tripId === this.tripId
+      }.bind(this)
+    )
+  }
+}
+
+///
+
+class Passenger {
+
+  constructor(name) {
+    this.name = name
+    this.id = ++passengerId
+
+    store.passengers.push(this)
+  }
+
+  trips() {
+    return store.trips.filter (
+      function(driver) {
+        return driver.tripId === this.tripId
+      }.bind(this)
+    )
+  }
+
+  drivers() {
+    return store.drivers.filter (
+      function(driver) {
+        return driver.tripId === this.tripId
+      }.bind(this)
+    )
+  }
+}
+
+///
+
+class Trip {
+
+  constructor(driver, passenger) {
+
+    this.driverId = driver.id
+    this.passengerId = passenger.id
+    this.id = ++tripId
+
+    store.trips.push(this)
+  }
+
+  passenger() {
+    return store.passengers.find(
+      function(passenger) {
+        passenger.tripId = this.id
+        passenger.driverId = this.driverId
+        return this.passengerId === passenger.id
+      }.bind(this)
+    )
+  }
+
+  driver() {
+    return store.drivers.find(
+      function(driver) {
+        driver.tripId = this.id
+        driver.passengerId = this.passengerId
+        return this.driverId === driver.id
+      }.bind(this)
+    )
+  }
+}
+
+let driver1 = new Driver('laura')
+let passenger1 = new Passenger('ryan')
+let driver2 = new Driver('avery')
+let passenger2 = new Passenger('tiki')
+
+let trip1 = new Trip(driver1, passenger2)
+let trip2 = new Trip(driver2, passenger1)
